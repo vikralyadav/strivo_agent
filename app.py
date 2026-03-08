ee60343b86b4943c4bd5f6e638f4906d886dc148
Vikral Yadav <yadavvikral2003@gmail.com>
2026-03-08 12:56:10 +0530

    database is created

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class QueryRequest(BaseModel):
+    query : str
+
+
+
+
+@app.post("/generate_sql")
+async def generate_sql(request: QueryRequest):
+    """Generate SQL query from natural language input."""
+
 
 
 
